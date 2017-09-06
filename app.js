e13ffdae9e39bc81213e1ce545058c376ca7f55f
ilgoo jung <idjung4@gmail.com>
2017-09-06 19:38:55 -0400

    process port

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:' + process.env.PORT);
 });
