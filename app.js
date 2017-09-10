78ac1d3440890999dc3daf1ba1696b6bba76d9c9
ilgoo jung <idjung4@gmail.com>
2017-09-10 17:11:22 -0400

    Page Styling Skeleton
    - page element dividors added
    - Need to fix canvas size

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+// Front-end files
+app.use(express.static('public'));
+
 
 app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
     // Create new rooms
-    res.send("Make new room");
 });
 
 app.get('/:room_id', function(req, res){
     // Check if room exists and join room
-    res.send("Join existing room " + req.params.room_id);
+    res.sendFile(__dirname + '/index.html');
+    //res.send("Join existing room " + req.params.room_id);
     // Else create new room
 });
-
-var port = process.env.PORT || 8080;
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
---
+++ public/connect4.js
@@ -0,0 +1,44 @@
+(function() {
+    var canvas = document.getElementById('canvas'),
+            ctx = canvas.getContext('2d');
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        //canvas.style.width='100%';
+        //canvas.style.height='100%';
+        //canvas.width  = canvas.offsetWidth;
+        //canvas.height = canvas.offsetHeight;
+        //canvas.width = document.getElementById('canvas-container').offsetWidth;
+        //canvas.height = document.getElementById('canvas-container').offsetHeight;
+
+        drawStuff(); 
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+        var backgroundColor = "rgb(50, 120, 225)";
+        var backgroundShade = "rgb(25, 60, 145)";
+        ctx.beginPath();
+        ctx.shadowBlur=5;
+        ctx.shadowColor= backgroundColor;
+        ctx.rect(0, 0,canvas.width, canvas.height);
+        ctx.fillStyle = backgroundColor;
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.shadowBlur=5;
+        ctx.shadowColor= backgroundShade;
+        ctx.arc(100,75,50,0,2*Math.PI);
+        ctx.fillStyle = backgroundShade;
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.shadowBlur=5;
+        ctx.shadowColor= backgroundColor;
+        ctx.arc(105,80,40,0,2*Math.PI);
+        ctx.fillStyle = backgroundColor;
+        ctx.fill();
+    }
+})();
