40d1c12c773feda9e790cdff0f2f127d869d053a
Blake <blakevolk@gmail.com>
2017-09-13 15:25:17 -0500

    Canvas Postitioning Fix + minor adjustments
    - Fixed canvas postioning bug
    - customBoard is now Board
    - Default board values set
    - Canvas color set to board background color

--- public/connect4.js
+++ public/connect4.js
@@ -1,15 +1,9 @@
-// Default board
-//var board = new Board(document.getElementById('canvas'));
-
-// Custom board
-var cBoard = new customBoard(document.getElementById('canvas'),14,10);
+// Default board (canvas, rows, columns)
+var board = new Board(document.getElementById('canvas'));
 
 // Redraw canvas at 50 fps (1000/50 ms)
 this.updateGame = setInterval(update, 20);
 function update() {
-    // board.clear();
-    // board.drawBoard();
-
-    cBoard.clear();
-    cBoard.drawBoard();
+    board.clear();
+    board.drawBoard();
 }
