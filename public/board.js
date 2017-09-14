0063bb5d00b1dd8582ac48e94b7282195411c548
Blake <blakevolk@gmail.com>
2017-09-14 14:56:51 -0500

    Player Pieces
    - Player pieces can now be placed on the board
    - Removed boardRows and boardCols (replaced by Board parameters)
    - The drawing for loop is initialized  by rows and columns instead of columns and rows.

--- public/board.js
+++ public/board.js
@@ -2,8 +2,6 @@ var Board = function(new_canvas, rows = 6, columns = 7) {
     var canvas = new_canvas;
     var ctx = canvas.getContext('2d');
     var cellLength;
-    var boardRows = rows;
-    var boardCols = columns;
     var boardWidth;
     var boardHeight;
     var backgroundColor = "rgb(50, 120, 225)"; //board color
@@ -12,14 +10,14 @@ var Board = function(new_canvas, rows = 6, columns = 7) {
     var piece2Color = "rgb(240, 101, 67)";
     var player1 = 1;
     var player2 = 2;
-    var board = [];
+    var board = [[]];
 
     // Initialize empty board
     for(var i = 0; i < rows; i++) {
+        board[i] = [];
         for(var j = 0; j < columns; j++)
             board[i][j] = 0;
     }
-    
 
     resizeCanvas();
 
@@ -29,9 +27,9 @@ var Board = function(new_canvas, rows = 6, columns = 7) {
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height  = window.innerHeight;
-        cellLength = Math.min(canvas.width/boardCols, canvas.height/boardRows);
-        boardWidth = cellLength*boardCols;
-        boardHeight = cellLength*boardRows;
+        cellLength = Math.min(canvas.height/rows, canvas.width/columns);
+        boardHeight = cellLength*rows;
+        boardWidth = cellLength*columns;
     }
 
     this.drawBoard = function() {
@@ -51,22 +49,31 @@ var Board = function(new_canvas, rows = 6, columns = 7) {
     
     function drawHoles(x, y){
         // Draw a hole at x and y, with the radius of the hole scale between 0-1 where 1 is the size of the cell
-        function drawHole(x, y, radiusScale) {
+        function drawHole(x, y, radiusScale, color) {
             var radius = cellLength/2 * radiusScale;
             x = x + cellLength/2;
             y = y + cellLength/2;
 
             ctx.beginPath();
-            ctx.shadowBlur=5;
-            ctx.shadowColor= backgroundColor;
-            ctx.arc(x, y, radius, 0, 2*Math.PI);
-            ctx.fillStyle = backgroundShade;
+            ctx.arc(y, x, radius, 0, 2*Math.PI);
+            ctx.fillStyle = color;
             ctx.fill();
         }
 
-        for(var i = 0; i < boardCols; i++) {
-            for(var j = 0; j < boardRows; j++) {
-                drawHole(i*cellLength + canvas.width/2 - boardWidth/2, j*cellLength + canvas.height - boardHeight, .1);
+        var horizontalOffset = (canvas.width - boardWidth)/2;
+        var verticalOffset = canvas.height - boardHeight;
+
+        for(var i = 0; i < rows; i++) {
+            for(var j = 0; j < columns; j++) {
+                if (board[i][j] === 1) {
+                    drawHole(i*cellLength + verticalOffset, j*cellLength + horizontalOffset, .1, piece1Color);
+                }
+                else if (board[i][j] === 2) {
+                    drawHole(i*cellLength + verticalOffset, j*cellLength + horizontalOffset, .1, piece2Color);
+                }
+                else {
+                    drawHole(i*cellLength + verticalOffset, j*cellLength + horizontalOffset, .1, backgroundShade);
+                }
             }
         }
     }
